f45479365958f1f396ce6628fea4720526c446b3
wzx88 <wuzhixiang88@gmail.com>
2021-10-16 23:19:28 +0800

    conditional rendering of navside bar based on logged in user role

--- client/src/components/NavSidebar.jsx
+++ client/src/components/NavSidebar.jsx
@@ -1,38 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const NavSidebar = () => {
-  return (
-    <div id="nav-sidebar">
-      <ul>
-        Guest Nav
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-      </ul>
-      <ul>
-        Customer Nav
-        <li>
-          <Link to="/customer">Book a Parcel</Link>
-        </li>
-        <li>
-          <Link to="/customerinbox">Inbox</Link>
-        </li>
-      </ul>
-      <ul>
-        Deliveryman Nav
-        <li>
-          <Link to="/deliveryman">Available Jobs</Link>
-        </li>
-        <li>
-          <Link to="/deliverymaninbox">Accepted Jobs</Link>
-        </li>
-      </ul>
-    </div>
-  );
+const NavSidebar = ({ loggedInUserRole }) => {
+  const [navSideBar, setNavSideBar] = useState();
+
+  useEffect(() => {
+    if (!loggedInUserRole) {
+      setNavSideBar(
+        <ul>
+          Guest Nav
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      );
+    } else if (loggedInUserRole.toLowerCase() === "customer") {
+      setNavSideBar(
+        <ul>
+          Customer Nav
+          <li>
+            <Link to="/customer">Book a Parcel</Link>
+          </li>
+          <li>
+            <Link to="/customerinbox">Inbox</Link>
+          </li>
+        </ul>
+      );
+    } else if (loggedInUserRole.toLowerCase() === "deliveryman") {
+      setNavSideBar(
+        <ul>
+          Deliveryman Nav
+          <li>
+            <Link to="/deliveryman">Available Jobs</Link>
+          </li>
+          <li>
+            <Link to="/deliverymaninbox">Inbox</Link>
+          </li>
+        </ul>
+      );
+    }
+  }, [loggedInUserRole]);
+
+  return <div id="nav-sidebar">{navSideBar}</div>;
 };
 
 export default NavSidebar;
