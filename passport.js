f1f8a8be2c95c9517a6a2aa2f570a119773a2db0
khairlazhr <khairlazhr@gmail.com>
2021-10-18 13:08:45 +0800

    backendstuff

--- passport.js
+++ passport.js
@@ -13,17 +13,25 @@ const cookieExtractor = (req) => {
 const options = {
   secretOrKey: process.env.SECRET_KEY_JWT,
   jwtFromRequest: cookieExtractor,
+  ignoreExpiration: true
 };
 
-const userStrategy = new Strategy(options, async (payload, done) => {
-  const user = await userModel.findOne({ username: payload.username });
-
-  if (!user) {
-    return done(null, false, { message: "User is not authorized"})
+const userStrategy = new Strategy(options, (payload, done) => {
+  if (new Date(payload.exp * 1000) < new Date()) {
+    return done(new Error("Token has expired"), null)
   }
-  if (user) {
-    return done(null, { username: user.username });
-  }
+  userModel.findOne({ username: payload.username}, (err, user) => {
+    if (err) {
+      return done(err, null)
+    }
+    if (!user) {
+      return done(null, false, { message: "User is not authorized"})
+    }
+    if (user) {
+      return done(null, { username: user.username, role: user.role });
+    }
+  })
 });
+
 
 module.exports = userStrategy;
